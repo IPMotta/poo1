56b73f273f34f2ed9b1b690996f0626b2c349338
IPMOTTA <tcmatheusmotta@hotmail.com>
2023-03-15 20:58:05 -0300

    Conta com saldo total

--- ControleContas/Program.cs
+++ ControleContas/Program.cs
@@ -3,4 +3,7 @@ using ControleContas.Model;
 
 Conta conta = new Conta("1234");
 Conta conta2 = new Conta("4321", 1000.99m);
-Console.WriteLine($"Conta: {conta.Numero}; Saldo: {conta.Saldo}");
+Conta Conta3 = new Conta("654321",2341.42m);
+
+Console.WriteLine($"Conta: {conta2.Numero}; Saldo: {conta2.Saldo}");
+Console.WriteLine($"O saldo total é: {conta2.SaldoTotal}");
